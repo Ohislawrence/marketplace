dda3f30ece54c3ffd022113e24d047c9b0c20c7e
Lawrence Ohis <lawrenceohis@gmail.com>
2022-12-09 16:56:18 +0100

    nofication added, still in work

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,19 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Notifications\CommentNotification;
+use Illuminate\Support\Facades\Notification;
 
 class CommentController extends Controller
 {
     public function comment(Request $request)
     {
+       
         if($request->comment)
         {
-            Comment::create([
+            $sent = Comment::create([
                 'user_id' => auth()->user()->id,
                 'product_id' => $request->productuuid,
                 'comment'=> $request->comment,
             ]);
+
+
+            $product = Product::where('slug', $request->productslug)->first();
+            $user = User::find($product->user_id);
+
+            $comment = [
+                'name' => 'Hello '.$user->name,
+                'body' => 'You have received a comment on your product, go to the link below to repy,',
+                'thanks' => 'Thank you.',
+                'actionText' => 'View Comment',
+                'actionURL' => route('singleproduct.page', ['productslug' => $request->productslug ]),
+                'product_id' => $request->productuuid
+            ];
+      
+            Notification::send($user, new CommentNotification($comment));
         }
 
 
@@ -58,5 +78,26 @@ class CommentController extends Controller
             ]);
         }
         return back();
+    }
+
+
+
+
+    public function sendNotification()
+    {
+        $user = User::first();
+  
+        $comment = [
+            'name' => 'Hello '.$user->name,
+            'body' => 'You have received a comment on your product, go to the link below to repy,',
+            'thanks' => 'Thank you.',
+            'actionText' => 'View Comment',
+            'actionURL' => url('/'),
+            'product_id' => 101
+        ];
+  
+        Notification::send($user, new CommentNotification($comment));
+   
+        dd('done');
     }
 }
